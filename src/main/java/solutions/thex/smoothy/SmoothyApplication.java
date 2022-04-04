729a103c796e18a04e4d0496bad53b9c0130b654
Soroush <s_shemshadi@ymail.com>
2022-04-04 10:05:52 +0430

    INIT(project) add a pom.xml based maven project management system and dependencies.

---
+++ src/main/java/solutions/thex/smoothy/SmoothyApplication.java
@@ -0,0 +1,13 @@
+package solutions.thex.smoothy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmoothyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solutions/thex/smoothy/SmoothyApplicationTests.java
@@ -0,0 +1,13 @@
+package solutions.thex.smoothy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
