11cec6fa3e1ed5c117cf5a45e8060a07fb5d8516
Soroush <s_shemshadi@ymail.com>
2022-04-05 16:02:51 +0430

    IMPLEMENT(AspectJConfig) Implement AspectJ configuration and enable AspectJAutoProxy.

---
+++ src/main/java/solutions/thex/smoothy/config/AspectJConfig.java
@@ -0,0 +1,10 @@
+package solutions.thex.smoothy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AspectJConfig {
+
+}
