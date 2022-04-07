a973de7d9bb4bf8217ea74588573eceba39876f2
Soroush <s_shemshadi@ymail.com>
2022-04-07 11:48:37 +0430

    IMPLEMENT(JavaCompilationUnit) Implement a class to represents an individual source file.

---
+++ src/main/java/solutions/thex/smoothy/code/java/JavaCompilationUnit.java
@@ -0,0 +1,39 @@
+package solutions.thex.smoothy.code.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+
+/**
+ * A Java-compilation unit that represents an individual source file.
+ */
+@Builder
+@Getter
+public class JavaCompilationUnit {
+
+	private final String packageName;
+
+	private final String name;
+
+	@Default
+	private final List<JavaTypeDeclaration> typeDeclarations = new ArrayList<>();
+
+	public JavaTypeDeclaration createTypeDeclaration(String name) {
+		JavaTypeDeclaration typeDeclaration = doCreateTypeDeclaration(name);
+		this.typeDeclarations.add(typeDeclaration);
+		return typeDeclaration;
+	}
+
+	public List<JavaTypeDeclaration> getTypeDeclarations() {
+		return Collections.unmodifiableList(this.typeDeclarations);
+	}
+
+	protected JavaTypeDeclaration doCreateTypeDeclaration(String name) {
+		return JavaTypeDeclaration.builder().name(name).build();
+	}
+
+}
