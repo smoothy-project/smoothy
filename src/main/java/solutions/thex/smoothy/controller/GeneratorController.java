be5f84ff4e3706ad97e79b499d430af8d9b4b5c2
Soroush <s_shemshadi@ymail.com>
2022-04-14 14:24:37 +0430

    UPDATE(GeneratorController) Update it to work with new SpringBootApplicationGenerator.

--- src/main/java/solutions/thex/smoothy/controller/GeneratorController.java
+++ src/main/java/solutions/thex/smoothy/controller/GeneratorController.java
@@ -9,47 +9,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-import solutions.thex.smoothy.code.JavaSourceCodeWriter;
 import solutions.thex.smoothy.generator.SpringBootApplicationGenerator;
-import solutions.thex.smoothy.zip.Zipper;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/generate")
 public class GeneratorController {
 
-    private final Path generationRootDirectory;
-    private final JavaSourceCodeWriter sourceWriter;
+    private final SpringBootApplicationGenerator generator;
 
     @Autowired
-    public GeneratorController(JavaSourceCodeWriter sourceWriter) throws URISyntaxException {
-        this.sourceWriter = sourceWriter;
-        this.generationRootDirectory = Paths.get("factory");
+    public GeneratorController(SpringBootApplicationGenerator generator) {
+        this.generator = generator;
     }
 
     @PostMapping(value = "", produces = "application/zip")
-    public ResponseEntity<StreamingResponseBody> generate(HttpServletRequest request, @RequestBody String payload) throws IOException {
-        SpringBootApplicationGenerator.builder()//
-                .application(new JSONObject(payload))//
-                .sourceWriter(sourceWriter)//
-                .generationRootDirectory(generationRootDirectory)//
-                .build()//
-                .generate();
-
+    public ResponseEntity<StreamingResponseBody> generate(HttpServletRequest request, @RequestBody String payload) {
         return ResponseEntity
                 .ok()
                 .header("Content-Disposition", "attachment; filename=\"smoothy.zip\"")
                 .body(out -> {
-                    Zipper.builder()//
-                            .source(generationRootDirectory.resolve("soroush"))//
-                            .build()//
-                            .zip(out);
+                    generator.generate(new JSONObject(payload), out);
                 });
     }
 
