60921296b0a39a446ae152e3a4f3345c1f07adf8
Soroush <s_shemshadi@ymail.com>
2022-04-16 09:51:28 +0430

    UPDATE(GeneratorController) Update it to map the given payload from user to an ApplicationDescription and then pass it to SpringBootApplicationGenerator.

--- src/main/java/solutions/thex/smoothy/controller/GeneratorController.java
+++ src/main/java/solutions/thex/smoothy/controller/GeneratorController.java
@@ -1,7 +1,7 @@
 package solutions.thex.smoothy.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-import solutions.thex.smoothy.generator.SpringBootApplicationGenerator;
+import solutions.thex.smoothy.generator.ApplicationDescription;
+import solutions.thex.smoothy.generator.spring.SpringBootApplicationGenerator;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,7 +32,7 @@ public class GeneratorController {
                 .ok()
                 .header("Content-Disposition", "attachment; filename=\"smoothy.zip\"")
                 .body(out -> {
-                    generator.generate(new JSONObject(payload), out);
+                    generator.generate(new ObjectMapper().readValue(payload, ApplicationDescription.class), out);
                 });
     }
 
