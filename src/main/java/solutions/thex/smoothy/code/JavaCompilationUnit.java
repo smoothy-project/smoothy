fa48462041c51597a14ce9ddaa43e7c2142516bb
Soroush <s_shemshadi@ymail.com>
2022-04-19 13:32:21 +0430

    UPDATE(JavaCompilationUnit) Implement a render interface for JavaCompilationUnit to write its own codes.

--- src/main/java/solutions/thex/smoothy/code/JavaCompilationUnit.java
+++ src/main/java/solutions/thex/smoothy/code/JavaCompilationUnit.java
@@ -4,17 +4,19 @@ import lombok.Builder;
 import lombok.Builder.Default;
 import lombok.Getter;
 import solutions.thex.smoothy.code.declaration.JavaTypeDeclaration;
+import solutions.thex.smoothy.code.formatting.IndentingWriter;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A Java-compilation unit that represents an individual source file.
  */
 @Builder
 @Getter
-public class JavaCompilationUnit {
+public class JavaCompilationUnit implements CompilationUnit{
 
     @Default
     private final List<JavaTypeDeclaration> typeDeclarations = new ArrayList<>();
@@ -23,6 +25,22 @@ public class JavaCompilationUnit {
 
     public List<JavaTypeDeclaration> getTypeDeclarations() {
         return Collections.unmodifiableList(this.typeDeclarations);
+    }
+
+    @Override
+    public void render(IndentingWriter writer) {
+        writer.println("package " + getPackageName() + ";");
+        writer.println();
+        Set<String> imports = JavaSourceCodeWriter.determineImports(this);
+        if (!imports.isEmpty()) {
+            for (String importedType : imports) {
+                writer.println("import " + importedType + ";");
+            }
+            writer.println();
+        }
+        for (JavaTypeDeclaration type : getTypeDeclarations()) {
+            type.render(writer);
+        }
     }
 
 }
