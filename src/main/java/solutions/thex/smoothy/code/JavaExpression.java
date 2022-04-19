75a4fbd15c793023aee7c39adead6c41fdd91e77
Soroush <s_shemshadi@ymail.com>
2022-04-19 13:49:00 +0430

    UPDATE(JavaExpression) Implement an interface for rendering code.

--- src/main/java/solutions/thex/smoothy/code/JavaExpression.java
+++ src/main/java/solutions/thex/smoothy/code/JavaExpression.java
@@ -1,8 +1,12 @@
 package solutions.thex.smoothy.code;
 
+import solutions.thex.smoothy.code.formatting.IndentingWriter;
+
 /**
  * A Java expression.
  */
 public interface JavaExpression {
+
+    void render(IndentingWriter writer);
 
 }
