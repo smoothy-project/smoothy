3833cd75f4fe9cc79a38a989059c5a7bacf65927
Soroush <s_shemshadi@ymail.com>
2022-04-19 13:43:41 +0430

    UPDATE(JavaExpressionStatement) Update it to a java record and implement a render method interface to JavaExpressionStatement renders its own code.

--- src/main/java/solutions/thex/smoothy/code/statement/JavaExpressionStatement.java
+++ src/main/java/solutions/thex/smoothy/code/statement/JavaExpressionStatement.java
@@ -4,14 +4,22 @@ import lombok.Builder;
 import lombok.Getter;
 import solutions.thex.smoothy.code.JavaExpression;
 import solutions.thex.smoothy.code.JavaStatement;
+import solutions.thex.smoothy.code.formatting.IndentingWriter;
 
 /**
  * A statement that contains a single expression.
  */
-@Builder
 @Getter
-public class JavaExpressionStatement implements JavaStatement {
+public record JavaExpressionStatement(JavaExpression expression) implements JavaStatement {
 
-    private final JavaExpression expression;
+    @Builder
+    public JavaExpressionStatement {
+    }
+
+    @Override
+    public void render(IndentingWriter writer) {
+        expression.render(writer);
+        writer.println(";");
+    }
 
 }
