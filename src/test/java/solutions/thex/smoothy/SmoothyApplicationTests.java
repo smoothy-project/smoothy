ad917d14ca24f7343a2e16f80ad479694f5f70d7
Soroush <s_shemshadi@ymail.com>
2022-04-19 20:12:59 +0430

    COMMENT(main classes) 0.1-DEV

--- src/test/java/solutions/thex/smoothy/SmoothyApplicationTests.java
+++ src/test/java/solutions/thex/smoothy/SmoothyApplicationTests.java
@@ -3,6 +3,9 @@ package solutions.thex.smoothy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * TODO: Implement all unit tests till version 0.2-DEV
+ */
 @SpringBootTest
 class SmoothyApplicationTests {
 
