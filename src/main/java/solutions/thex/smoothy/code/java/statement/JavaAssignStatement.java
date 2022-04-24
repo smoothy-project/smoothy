a2f711172df88eb328fba08d8d52c3f28b6b1445
Soroush <s_shemshadi@ymail.com>
2022-04-24 10:15:42 +0430

    UPDATE && TEST(JavaAssignStatement) Repackage. Update render method. Test render method.

---
+++ src/main/java/solutions/thex/smoothy/code/java/statement/JavaAssignStatement.java
@@ -0,0 +1,20 @@
+package solutions.thex.smoothy.code.java.statement;
+
+import lombok.Builder;
+import solutions.thex.smoothy.code.Expression;
+import solutions.thex.smoothy.code.Statement;
+
+
+public record JavaAssignStatement(String variable,
+                                  Expression expression) implements Statement {
+
+    @Builder
+    public JavaAssignStatement {
+    }
+
+    @Override
+    public String render() {
+        return this.variable + " = " + this.expression.render();
+    }
+
+}
---
+++ src/test/java/solutions/thex/smoothy/code/java/statement/JavaAssignStatementTests.java
@@ -0,0 +1,31 @@
+package solutions.thex.smoothy.code.java.statement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import solutions.thex.smoothy.code.java.expression.JavaPlainValueExpression;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class JavaAssignStatementTests {
+
+    private JavaAssignStatement javaAssignStatement;
+
+    @Test
+    void javaAssignStatement_should_render_correct_statement() {
+        // Given
+        javaAssignStatement = JavaAssignStatement.builder()//
+                .variable("variable")
+                .expression(JavaPlainValueExpression.builder()//
+                        .value("value")//
+                        .build())//
+                .build();
+
+        // When
+        String statement = javaAssignStatement.render();
+
+        // Then
+        assertEquals("variable = value", statement);
+    }
+
+}
