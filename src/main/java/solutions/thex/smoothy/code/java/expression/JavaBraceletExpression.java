6ca4513dae8454f4f258420bfaca044c168f6a13
Soroush <s_shemshadi@ymail.com>
2022-04-24 09:18:09 +0430

    IMPLEMENT && TEST(JavaBraceletExpression) Implement a class which represent java bracelet expressions, ex: (1 + 2), (1 + (3 - 2)), (true || false), ... Test render method and operand.

---
+++ src/main/java/solutions/thex/smoothy/code/java/expression/JavaBraceletExpression.java
@@ -0,0 +1,27 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import solutions.thex.smoothy.code.Expression;
+import solutions.thex.smoothy.code.java.Operable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuperBuilder
+@Getter
+public class JavaBraceletExpression extends Operable implements Expression {
+
+    @Builder.Default
+    List<Expression> expressions = new LinkedList<>();
+    @Builder.Default
+    private boolean bracelet = true;
+
+    @Override
+    public String render() {
+        return ((bracelet) ? "(" : "") + expressions.stream().map(Expression::render).collect(Collectors.joining("")) + ((bracelet) ? ")" : "");
+    }
+
+}
---
+++ src/test/java/solutions/thex/smoothy/code/java/expression/JavaBraceletExpressionTests.java
@@ -0,0 +1,127 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import solutions.thex.smoothy.code.java.JavaOperand;
+import solutions.thex.smoothy.code.java.MethodInvoke;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class JavaBraceletExpressionTests {
+
+
+    private JavaBraceletExpression javaBraceletExpression;
+
+    @Test
+    void javaPlainValueExpression_with_one_javaPlainValueExpression_should_render_correct_expression() {
+        // Given
+        javaBraceletExpression = JavaBraceletExpression.builder()//
+                .expressions(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .value("1") //
+                                .build()//
+                ))
+                .build();
+
+        // When
+        String expression = javaBraceletExpression.render();
+
+        // Then
+        assertEquals("(1)", expression);
+    }
+
+    @Test
+    void javaPlainValueExpression_with_one_javaPlainValueExpression_without_bracelet_should_render_correct_expression() {
+        // Given
+        javaBraceletExpression = JavaBraceletExpression.builder()//
+                .bracelet(false)//
+                .expressions(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .value("1") //
+                                .build()//
+                ))
+                .build();
+
+        // When
+        String expression = javaBraceletExpression.render();
+
+        // Then
+        assertEquals("1", expression);
+    }
+
+    @Test
+    void javaPlainValueExpression_with_one_javaDotClassExpression_should_render_correct_expression() {
+        // Given
+        javaBraceletExpression = JavaBraceletExpression.builder()//
+                .expressions(List.of(//
+                        JavaDotClassExpression.builder()//
+                                .name("JavaDotClassExpression") //
+                                .build()//
+                ))
+                .build();
+
+        // When
+        String expression = javaBraceletExpression.render();
+
+        // Then
+        assertEquals("(JavaDotClassExpression.class)", expression);
+    }
+
+    @Test
+    void javaPlainValueExpression_with_multiple_expressions_should_render_correct_expression() {
+        // Given
+        javaBraceletExpression = JavaBraceletExpression.builder()//
+                .expressions(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .operand(JavaOperand.PLUS)
+                                .value("1") //
+                                .build(),//
+                        JavaPlainValueExpression.builder()//
+                                .value("2") //
+                                .build()//
+                ))
+                .build();
+
+        // When
+        String expression = javaBraceletExpression.render();
+
+        // Then
+        assertEquals("(1 + 2)", expression);
+    }
+
+    @Test
+    void javaPlainValueExpression_with_nested_bracelet_should_render_correct_expression() {
+        // Given
+        javaBraceletExpression = JavaBraceletExpression.builder()//
+                .expressions(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .operand(JavaOperand.AND)
+                                .value("true") //
+                                .build(),//
+                        JavaBraceletExpression.builder()//
+                                .expressions(List.of(//
+                                        JavaNewInstanceExpression.builder()//
+                                                .name("java.util.Date")//
+                                                .invokes(List.of(//
+                                                        MethodInvoke.builder()//
+                                                                .method("after")//
+                                                                .arguments(List.of(//
+                                                                        JavaNewInstanceExpression.builder()//
+                                                                                .name("java.util.Date")//
+                                                                                .build()))//
+                                                                .build()))//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaBraceletExpression.render();
+
+        // Then
+        assertEquals("(true && (new Date().after(new Date())))", expression);
+    }
+
+}
