ebd123fd4ff341e7d81638811ba074ebb12a7771
Soroush <s_shemshadi@ymail.com>
2022-04-24 10:05:47 +0430

    UPDATE && TEST(JavaMethodInvocationExpression) Repackage. Extract MethodInvoke inner class. Update render method. Test render method and operand.

---
+++ src/main/java/solutions/thex/smoothy/code/java/expression/JavaMethodInvocationExpression.java
@@ -0,0 +1,32 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import solutions.thex.smoothy.code.Expression;
+import solutions.thex.smoothy.code.java.JavaSourceCodeWriter;
+import solutions.thex.smoothy.code.java.MethodInvoke;
+import solutions.thex.smoothy.code.java.Operable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * An invocation of a method.
+ */
+@SuperBuilder
+@Getter
+public class JavaMethodInvocationExpression extends Operable implements Expression {
+
+    @Builder.Default
+    private final List<MethodInvoke> invokes = new LinkedList<>();
+    private final String target;
+
+    @Override
+    public String render() {
+        return JavaSourceCodeWriter.getUnqualifiedName(this.target)//
+                + invokes.stream().map(MethodInvoke::render).collect(Collectors.joining("")) + super.render();
+    }
+
+}
---
+++ src/test/java/solutions/thex/smoothy/code/java/expression/JavaMethodInvocationExpressionTests.java
@@ -0,0 +1,148 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import solutions.thex.smoothy.code.java.JavaOperand;
+import solutions.thex.smoothy.code.java.MethodInvoke;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class JavaMethodInvocationExpressionTests {
+
+    private JavaMethodInvocationExpression javaMethodInvocationExpression;
+
+    @Test
+    void javaMethodInvocationExpression_with_one_methodInvoke_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test.method()", expression);
+    }
+
+    @Test
+    void javaMethodInvocationExpression_with_one_methodInvoke_and_operand_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test")//
+                .operand(JavaOperand.PLUS)
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test.method() + ", expression);
+    }
+
+    @Test
+    void javaMethodInvocationExpression_with_one_methodInvoke_and_argument_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method")//
+                                .arguments(List.of(
+                                        JavaPlainValueExpression.builder()//
+                                                .value("arg1")//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test.method(arg1)", expression);
+    }
+
+    @Test
+    void javaMethodInvocationExpression_with_one_methodInvoke_multiple_arguments_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method")//
+                                .arguments(List.of(
+                                        JavaPlainValueExpression.builder()//
+                                                .value("arg1")//
+                                                .build(),//
+                                        JavaPlainValueExpression.builder()//
+                                                .value("arg2")//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test.method(arg1, arg2)", expression);
+    }
+
+    @Test
+    void javaMethodInvocationExpression_with_nested_methodInvokes_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test1")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method1")//
+                                .arguments(List.of(//
+                                        JavaMethodInvocationExpression.builder()//
+                                                .target("test2")//
+                                                .invokes(List.of(//
+                                                        MethodInvoke.builder()//
+                                                                .method("method2")//
+                                                                .build()))//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test1.method1(test2.method2())", expression);
+    }
+
+    @Test
+    void javaMethodInvocationExpression_with_multiple_methodInvokes_should_render_correct_expression() {
+        // Given
+        javaMethodInvocationExpression = JavaMethodInvocationExpression.builder()//
+                .target("test")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("method1")//
+                                .build(),//
+                        MethodInvoke.builder()//
+                                .method("method2")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaMethodInvocationExpression.render();
+
+        // Then
+        assertEquals("test.method1().method2()", expression);
+    }
+
+}
