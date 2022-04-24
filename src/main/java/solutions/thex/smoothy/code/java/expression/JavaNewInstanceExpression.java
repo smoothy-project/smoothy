f350251696af760fcdd8a28e7ef181338d56ecde
Soroush <s_shemshadi@ymail.com>
2022-04-24 09:10:48 +0430

    UPDATE && TEST(JavaObjectNewInstanceExpression To JavaNewInstanceExpression) Rename and Update render method. Test render and operand.

---
+++ src/main/java/solutions/thex/smoothy/code/java/expression/JavaNewInstanceExpression.java
@@ -0,0 +1,39 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import solutions.thex.smoothy.code.Expression;
+import solutions.thex.smoothy.code.java.JavaSourceCodeWriter;
+import solutions.thex.smoothy.code.java.MethodInvoke;
+import solutions.thex.smoothy.code.java.Operable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuperBuilder
+@Getter
+public class JavaNewInstanceExpression extends Operable implements Expression {
+
+    @Builder.Default
+    private final List<Expression> arguments = new LinkedList<>();
+    @Builder.Default
+    private final List<MethodInvoke> invokes = new LinkedList<>();
+    private final String name;
+
+    @Override
+    public String render() {
+        return "new " + JavaSourceCodeWriter.getUnqualifiedName(this.name)//
+                + "("//
+                + this.arguments.stream().map(Expression::render).collect(Collectors.joining(", "))//
+                + ")"//
+                + renderInvokes()//
+                + super.render();
+    }
+
+    private String renderInvokes() {
+        return (invokes.isEmpty()) ? "" : ("." + this.invokes.stream().map(MethodInvoke::render).collect(Collectors.joining("")));
+    }
+
+}
---
+++ src/test/java/solutions/thex/smoothy/code/java/expression/JavaNewInstanceExpressionTests.java
@@ -0,0 +1,159 @@
+package solutions.thex.smoothy.code.java.expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import solutions.thex.smoothy.code.java.JavaOperand;
+import solutions.thex.smoothy.code.java.MethodInvoke;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class JavaNewInstanceExpressionTests {
+
+    private JavaNewInstanceExpression javaNewInstanceExpression;
+
+    @Test
+    void javaNewInstanceExpression_with_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("Test")//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Test()", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_one_argument_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("Test")//
+                .arguments(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .value("1")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Test(1)", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_nested_javaNewInstanceExpression_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("Test")//
+                .arguments(List.of(//
+                        JavaNewInstanceExpression.builder()//
+                                .name("Test")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Test(new Test())", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_nested_argument_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("Test")//
+                .arguments(List.of(//
+                        JavaMethodInvocationExpression.builder()//
+                                .target("someObject")//
+                                .invokes(List.of(//
+                                        MethodInvoke.builder()//
+                                                .method("getValue")//
+                                                .arguments(List.of(//
+                                                        JavaPlainValueExpression.builder()//
+                                                                .value("1")//
+                                                                .build()))//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Test(someObject.getValue(1))", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_multiple_argument_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("Test")//
+                .arguments(List.of(//
+                        JavaPlainValueExpression.builder()//
+                                .value("1")//
+                                .build(),//
+                        JavaPlainValueExpression.builder()//
+                                .value("true")//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Test(1, true)", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_invokes_and_nested_javaNewInstanceExpression_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("java.util.Date")//
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("after")//
+                                .arguments(List.of(//
+                                        JavaNewInstanceExpression.builder()//
+                                                .name("java.util.Date")//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Date().after(new Date())", expression);
+    }
+
+    @Test
+    void javaNewInstanceExpression_with_operand_should_render_correct_expression() {
+        // Given
+        javaNewInstanceExpression = JavaNewInstanceExpression.builder()//
+                .name("java.util.Date")//
+                .operand(JavaOperand.AND)
+                .invokes(List.of(//
+                        MethodInvoke.builder()//
+                                .method("after")//
+                                .arguments(List.of(//
+                                        JavaNewInstanceExpression.builder()//
+                                                .name("java.util.Date")//
+                                                .build()))//
+                                .build()))//
+                .build();
+
+        // When
+        String expression = javaNewInstanceExpression.render();
+
+        // Then
+        assertEquals("new Date().after(new Date()) && ", expression);
+    }
+
+}
