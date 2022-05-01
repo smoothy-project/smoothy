9d89d32669e65bfefc34037e8f2f577d8040fd16
Soroush <s_shemshadi@ymail.com>
2022-05-01 11:04:14 +0430

    IMPLEMENT(ExceptionHandlerFilterGenerator)

--- src/main/java/solutions/thex/smoothy/generator/spring/SpringBootApplicationGenerator.java
+++ src/main/java/solutions/thex/smoothy/generator/spring/SpringBootApplicationGenerator.java
@@ -12,7 +12,10 @@ import solutions.thex.smoothy.generator.ApplicationDescription;
 import solutions.thex.smoothy.generator.spring.conf.SmoothyDotConfFileGenerator;
 import solutions.thex.smoothy.generator.spring.main.src.java.MainClassGenerator;
 import solutions.thex.smoothy.generator.spring.main.src.java.security.SecurityConfigGenerator;
-import solutions.thex.smoothy.generator.spring.main.src.java.security.jwt.JwtConfigsGenerators;
+import solutions.thex.smoothy.generator.spring.main.src.java.security.UnauthorizedEntryPointGenerator;
+import solutions.thex.smoothy.generator.spring.main.src.java.security.filter.ExceptionHandlerFilterGenerator;
+import solutions.thex.smoothy.generator.spring.main.src.java.security.filter.JwtAuthenticationFilterGenerator;
+import solutions.thex.smoothy.generator.spring.main.src.java.security.jwt.JwtGenerator;
 import solutions.thex.smoothy.generator.spring.main.src.resources.ApplicationPropertiesFileGenerator;
 import solutions.thex.smoothy.generator.spring.main.test.MainClassTestsGenerator;
 import solutions.thex.smoothy.generator.spring.pom.PomFileGenerator;
@@ -45,8 +48,11 @@ public class SpringBootApplicationGenerator {
     private List<JavaCompilationUnit> generateCompilationUnits(ApplicationDescription application) {
         List<JavaCompilationUnit> compilationUnits = new ArrayList<>();
         compilationUnits.add(MainClassGenerator.generate(application.getName()));
-        compilationUnits.addAll(SecurityConfigGenerator.generate(application.getName()));
-        compilationUnits.addAll(JwtConfigsGenerators.generate(application.getName()));
+        compilationUnits.add(SecurityConfigGenerator.generate(application.getName()));
+        compilationUnits.add(UnauthorizedEntryPointGenerator.generate(application.getName()));
+        compilationUnits.add(JwtGenerator.generate(application.getName()));
+        compilationUnits.add(JwtAuthenticationFilterGenerator.generate(application.getName()));
+        compilationUnits.add(ExceptionHandlerFilterGenerator.generate(application.getName()));
         return compilationUnits;
     }
 
