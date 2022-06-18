4ad952b107117379762b8bc65afd16ce89ebc4f1
Soroush <s_shemshadi@ymail.com>
2022-06-18 16:16:49 +0430

    UPDATE(Application) Add generate function interface. Add getName to force child classes to have name filed.

--- src/main/java/solutions/thex/smoothy/generator/Application.java
+++ src/main/java/solutions/thex/smoothy/generator/Application.java
@@ -1,4 +1,11 @@
 package solutions.thex.smoothy.generator;
 
+import java.io.OutputStream;
+
 public interface Application {
+
+    String getName();
+
+    void generate(OutputStream out);
+
 }
--- src/main/java/solutions/thex/smoothy/generator/ApplicationGenerator.java
+++ src/main/java/solutions/thex/smoothy/generator/ApplicationGenerator.java
@@ -1,9 +1,6 @@
 package solutions.thex.smoothy.generator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import solutions.thex.smoothy.generator.spring.SpringBootApplication;
-import solutions.thex.smoothy.generator.spring.SpringBootApplicationGenerator;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -11,17 +8,8 @@ import java.io.OutputStream;
 @Service
 public class ApplicationGenerator {
 
-    private final SpringBootApplicationGenerator springGenerator;
-
-    @Autowired
-    public ApplicationGenerator(SpringBootApplicationGenerator springGenerator) {
-        this.springGenerator = springGenerator;
-    }
-
     public void generate(ApplicationDescription application, OutputStream out) throws IOException {
-        if (application.getApplication() instanceof SpringBootApplication)
-            springGenerator.generate(application, out);
-
+        application.getApplication().generate(out);
     }
 
 }
