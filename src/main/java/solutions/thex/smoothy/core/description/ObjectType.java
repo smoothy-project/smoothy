b95023095a337912709e6ff73e47263432911b23
Soroush <s_shemshadi@ymail.com>
2022-06-22 16:33:10 +0430

    UPDATE(ObjectType) Store package name of each object type as property of enum.

--- src/main/java/solutions/thex/smoothy/core/description/ObjectType.java
+++ src/main/java/solutions/thex/smoothy/core/description/ObjectType.java
@@ -2,6 +2,24 @@ package solutions.thex.smoothy.core.description;
 
 public enum ObjectType {
 
-        DAO, REPOSITORY, SERVICE, CONTROLLEER
+    DAO("dao"),//
+    REPOSITORY("dao.repository"),//
+    SERVICE("dao.service"),//
+    CONTROLLEER("controller");
+
+    private final String packageName;
+
+    ObjectType(String packageName) {
+        this.packageName = packageName;
+    }
+
+    @Override
+    public String toString() {
+        return packageName.split("\\.")[packageName.split("\\.").length - 1];
+    }
+
+    public String getPackageName(){
+        return packageName;
+    }
 
 }
