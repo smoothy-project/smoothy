947cfce9747ca3c57941b83ee9efc5bfc1d0aeac
Soroush <s_shemshadi@ymail.com>
2022-06-22 16:35:37 +0430

    UPDATE(StringFormatter) Implement new method for generating pascal case string base on array of strings.

--- src/main/java/solutions/thex/smoothy/util/StringFormatter.java
+++ src/main/java/solutions/thex/smoothy/util/StringFormatter.java
@@ -1,13 +1,21 @@
 package solutions.thex.smoothy.util;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class StringFormatter {
 
     public static String toPascalCase(String string) {
-        return string.substring(0, 1).toUpperCase() + string.substring(1);
+        return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+    }
+
+    public static String toPascalCase(String... strings) {
+        return Arrays.stream(strings).map(StringFormatter::toPascalCase).collect(Collectors.joining());
     }
 
     public static String toCamelCase(String string) {
-        return string.substring(0, 1).toLowerCase() + string.substring(1);
+        return string.substring(0, 1).toLowerCase() + string.substring(1).toLowerCase(Locale.ROOT);
     }
 
     public static String camelToKebab(String input) {
