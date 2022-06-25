36a81e7caf5902e2cfd94a86a7f0989460682cdb
Soroush <s_shemshadi@ymail.com>
2022-06-25 10:39:50 +0430

    UPDATE(JavaDAOTypeDescription) Add two field `storeCreatedDate` && `storeUpdatedDate` and assign initial value of them.

--- src/main/java/solutions/thex/smoothy/core/description/java/type/JavaDAOTypeDescription.java
+++ src/main/java/solutions/thex/smoothy/core/description/java/type/JavaDAOTypeDescription.java
@@ -1,16 +1,19 @@
 package solutions.thex.smoothy.core.description.java.type;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import solutions.thex.smoothy.core.description.java.JavaTypeDescription;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
-//@AllArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 public final class JavaDAOTypeDescription extends JavaTypeDescription {
+
+    @Builder.Default
+    private boolean storeCreatedDate = false;
+    @Builder.Default
+    private boolean storeUpdatedDate = false;
+
 }
