c6c979d972540eacc6a7945b77e0eca9634cfaee
Soroush <s_shemshadi@ymail.com>
2022-06-25 10:05:51 +0430

    IMPLEMENT(JavaFieldDescription) Implement an abstract class to represent fields of a java type.

---
+++ src/main/java/solutions/thex/smoothy/core/description/java/field/JavaRepositoryFieldDescription.java
@@ -0,0 +1,15 @@
+package solutions.thex.smoothy.core.description.java.field;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import solutions.thex.smoothy.core.description.java.JavaFieldDescription;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+//@AllArgsConstructor
+@NoArgsConstructor
+public final class JavaRepositoryFieldDescription extends JavaFieldDescription {
+}
